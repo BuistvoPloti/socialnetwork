2581685c91c264284fa5141e466c83d7cb346024
DooDac LongNhat <32066150+BuistvoPloti@users.noreply.github.com>
2020-01-16 20:38:54 +0200

    components added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/938d0a9202421.560ca5fe55ca5.png' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img className='bg' src='http://walldiskpaper.com/wp-content/uploads/2014/10/Sea-Wallpaper-Traveling-Holiday-Sea.jpg' />
+        </div>
+        <div>
+          ava
+        </div>
+        <div>
+          posts
+          <div>
+            new post
+          </div>
+          <div>
+            post 1 
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
