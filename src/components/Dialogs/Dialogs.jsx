a3fea68f666a520a6291993df7b7e2c3c8caddfa
DooDac LongNhat <32066150+BuistvoPloti@users.noreply.github.com>
2020-01-17 19:50:19 +0200

    dialogs page changes
    dialogs css improvements layout

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
-    <div > 
-      dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog +' '+ s.active}>
+            oskar ezih
+        </div>
+        <div className={s.dialog}>
+            zazynim maksar
+        </div>
+        <div className={s.dialog}>
+            oleg matuga
+        </div>
+        <div className={s.dialog}>
+            sanya kalanduk
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Hru rn?</div>
+        <div className={s.message}>Missin u rn</div>
+      </div>
     </div>
   )
 }
