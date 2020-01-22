861552040be702a31b2d98c3316b1b32931214d2
DooDac LongNhat <32066150+BuistvoPloti@users.noreply.github.com>
2020-01-22 14:53:43 +0200

    store v1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
-import { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost } from './redux/state';
-import { updateNewPostText } from './redux/state';
 
-let rernderEntireTree = (state) => {
+let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+      <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
     </BrowserRouter>, document.getElementById('root'));
 }
 
+rerenderEntireTree(store.getState())
 
-rernderEntireTree(state)
-
-subscribe(rernderEntireTree) 
+store.subscribe(rerenderEntireTree) 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,58 +1,58 @@
-let rerenderEntireTree = () => {
-
-}
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'haro', likesCount: 24 },
-      { id: 2, message: 'priver', likesCount: 11 }
-    ],
-    newPostText: 'myuuuuiiii'
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'haro', likesCount: 24 },
+        { id: 2, message: 'priver', likesCount: 11 }
+      ],
+      newPostText: 'myuuuuiiii'
+    },
+    dialogsPage: {
+      messages: [
+        { id: 1, message: 'haro' },
+        { id: 2, message: 'priver' },
+        { id: 3, message: 'okvha' },
+        { id: 4, message: 'hru rn' }
+      ],
+      dialogs: [
+        { id: 1, name: 'oskar ezih', ava:'https://i.ytimg.com/vi/BNcxTNrtRdk/maxresdefault.jpg' },
+        { id: 2, name: 'maksis zazinim', ava:'https://i.ytimg.com/vi/BNcxTNrtRdk/maxresdefault.jpg' },
+        { id: 3, name: 'artem barabashkinzov', ava:'https://i.ytimg.com/vi/BNcxTNrtRdk/maxresdefault.jpg' },
+        { id: 4, name: 'arbar2000bytkevuchys', ava:'https://i.ytimg.com/vi/BNcxTNrtRdk/maxresdefault.jpg' }
+      ]
+    },
+    friends: {
+      topThreeFriends: [
+        { id: 1, name: 'sanya kakashnik', ava:'https://www.okino.ua/media/var/news/2018/10/04/venom.jpg' },
+        { id: 2, name: 'grigoriy libidko', ava:'https://www.vbetnews.com/wp-content/uploads/2020/01/EFBEaaSXYAMMIl2.jpg' },
+        { id: 3, name: 'dimuch', ava:'https://pbs.twimg.com/profile_images/1106202296801595392/99Ovyotd_400x400.jpg' },
+      ]
+    }
   },
-  dialogsPage: {
-    messages: [
-      { id: 1, message: 'haro' },
-      { id: 2, message: 'priver' },
-      { id: 3, message: 'okvha' },
-      { id: 4, message: 'hru rn' }
-    ],
-    dialogs: [
-      { id: 1, name: 'oskar ezih', ava:'https://i.ytimg.com/vi/BNcxTNrtRdk/maxresdefault.jpg' },
-      { id: 2, name: 'maksis zazinim', ava:'https://i.ytimg.com/vi/BNcxTNrtRdk/maxresdefault.jpg' },
-      { id: 3, name: 'artem barabashkinzov', ava:'https://i.ytimg.com/vi/BNcxTNrtRdk/maxresdefault.jpg' },
-      { id: 4, name: 'arbar2000bytkevuchys', ava:'https://i.ytimg.com/vi/BNcxTNrtRdk/maxresdefault.jpg' }
-    ]
+  getState() {
+    return this._state
   },
-  friends: {
-    topThreeFriends: [
-      { id: 1, name: 'sanya kakashnik', ava:'https://www.okino.ua/media/var/news/2018/10/04/venom.jpg' },
-      { id: 2, name: 'grigoriy libidko', ava:'https://www.vbetnews.com/wp-content/uploads/2020/01/EFBEaaSXYAMMIl2.jpg' },
-      { id: 3, name: 'dimuch', ava:'https://pbs.twimg.com/profile_images/1106202296801595392/99Ovyotd_400x400.jpg' },
-    ]
+  _callSubscriber() {
+  },
+  addPost() {
+    let newPost = {
+    id: 4,
+      message: this._state.profilePage.newPostText,
+      likesCount: 13
+    }
+    this._state.profilePage.posts.push(newPost)
+    this._state.profilePage.newPostText=''
+    this._callSubscriber(this._state)
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText
+    this._callSubscriber(this._state)
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer
   }
 }
 
-window.state = state
+window.store = store
 
-export const addPost = () => {
-  let newPost = {
-  id: 4,
-    message: state.profilePage.newPostText,
-    likesCount: 13
-  }
-  state.profilePage.posts.push(newPost)
-  state.profilePage.newPostText=''
-  rerenderEntireTree(state)
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText
-  rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer
-}
-
-export default state;
+export default store;
