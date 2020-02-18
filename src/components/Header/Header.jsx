9a700fe38351ede0498b63a749c595837a1bb956
DooDac LongNhat <mister.postmodern@gmail.com>
2020-02-18 13:51:25 +0200

    login 1/2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom"
 
 const Header = () => {
 	return (
 		<header className={s.header}>
 			<img src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/938d0a9202421.560ca5fe55ca5.png' />
+			<div className={s.loginBlock}>
+			<NavLink to={'/login'}> Login </NavLink>
+			</div>
 		</header>
 	);
 }
-
+//21"33
 export default Header;
