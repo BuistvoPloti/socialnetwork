89bebd572f85aadc4a59ed10f7316b8810e02716
DooDac LongNhat <mister.postmodern@gmail.com>
2020-03-16 12:34:39 +0200

    reselector

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,5 +1,15 @@
-export const getUsers = (state) => {
+import {createSelector} from "reselect"
+
+const getUsersSelector = (state) => {
   return state.usersPage.users
+}
+
+export const getUsers = createSelector(getUsersSelector, (users) => {
+  return users.filter(u => true)  //fake filter shrug
+})
+
+export const getIsFetching = (state) => {
+  return state.usersPage.isFetching
 }
 
 export const getTotalUsersCount = (state) => {
@@ -12,10 +22,6 @@ export const getPageSize = (state) => {
 
 export const getCurrentPage = (state) => {
   return state.usersPage.currentPage
-}
-
-export const getIsFetching = (state) => {
-  return state.usersPage.isFetching
 }
 
 export const getFollowingInProgress = (state) => {
